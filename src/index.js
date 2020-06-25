eb4a4b896a2485fd252a53e2071004234b9e7767
David Park <david.park@prosearch.us>
2020-06-24 18:12:48 -0700

    setup webpack with react

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React, { Component} from "react";
+import ReactDOM from "react-dom";
+
+class Form extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            value: ""
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        const {
+            value
+        } = event.target;
+        this.setState(() => {
+            return {
+                value
+            };
+        });
+    }
+
+    render() {
+        return (<div>Hello World</div> );
+    }
+}
+
+export default Form;
+
+const wrapper = document.getElementById("root");
+wrapper ? ReactDOM.render( < Form / > , wrapper) : false;
